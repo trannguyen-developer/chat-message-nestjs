1ef1932f9e0fda8a50e81907ae2d7d2b7f4aa6c8
Nguyen Tran <nguyen.tran@golfzon.com>
2024-11-23 13:34:06 +0700

    Feat: Trigger build vercel

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
-
+// update test
 @Module({
   providers: [RedisService], // Khai báo provider
   exports: [RedisService], // Để sử dụng ở module khác
